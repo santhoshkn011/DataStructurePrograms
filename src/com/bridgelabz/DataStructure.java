e5f3f090bc3f9c7c7ff905c1939183a4cebd6bcb
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-06 15:35:17 +0530

    Added: Welcome Message added

---
+++ src/com/bridgelabz/DataStructure.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Data Structure program.");
+    }
+}
